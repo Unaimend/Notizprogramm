c881c5999a5f959a1122662aaaaf22d5568df884
tdDebian <Unaimend@gmail.com>
2016-01-04 03:01:15 +0100

    Logik fuer das Textauslesen hinzugefugt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,13 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->listView->setViewMode(QListView::ListMode);
+
+
+    mNoteListModel = new QStandardItemModel();
+    ui->listView->setModel(mNoteListModel);
+
+
 
     if(!QDir("notes").exists())
     {
@@ -14,11 +21,6 @@ MainWindow::MainWindow(QWidget *parent) :
     }
 
     dirIt = new QDirIterator(QDir::currentPath() + "/notes", QDirIterator::Subdirectories);
-
-
-
-
-
 
     while (dirIt->hasNext())
     {
@@ -30,8 +32,24 @@ MainWindow::MainWindow(QWidget *parent) :
     }
     for(auto& it : mNotes)
     {
+        QStandardItem* Items = new QStandardItem(it.mNoteName);
+
+        mNoteListModel->appendRow(Items);
         qDebug() << it.mNotePath;
     }
+
+
+
+
+
+    //BUG FALLS KEINE NOTES DA
+    while (!mNotes[0].mNoteText->atEnd())
+    {
+        QString line = mNotes[0].mNoteText->readLine();
+        ui->textEdit->append(line);
+    }
+
+
 }
 MainWindow::~MainWindow()
 {
@@ -40,3 +58,24 @@ MainWindow::~MainWindow()
 
 
 
+
+void MainWindow::on_listView_pressed(const QModelIndex &index)
+{
+    for(auto& it : mNotes)
+    {
+        if(it.mNoteName == mNoteListModel->data(index).toString())
+        {
+            ui->textEdit->setText("");
+            it.mNoteText->seek(0);
+            while (!it.mNoteText->atEnd())
+            {
+                QString line = it.mNoteText->readLine();
+                ui->textEdit->append(line);
+            }
+        }
+        else
+        {
+
+        }
+    }
+}

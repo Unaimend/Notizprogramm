788c4f3877c0e4f5032aa2aa3e0e470b291ea1cf
tdDebian <Unaimend@gmail.com>
2016-01-04 03:03:23 +0100

    Gui-Widgets hinzugefuegt

--- Note.cpp
+++ Note.cpp
@@ -7,4 +7,13 @@ Note::Note(const QString& notepath)
     //gefunden wird, und gibt einen Teilstring ab da zurueck.
     //In diesem Fall de Namen der Notiz
     mNoteName = mNotePath.section("/", -1);
+
+    //Neue FileStream erstellen
+    mNoteData = new QFile(mNotePath);
+    //Datei oeffnen
+    if (!mNoteData->open(QIODevice::ReadWrite | QIODevice::Text))
+           return;
+    //Textstrean fuer zugriff benutzen
+    mNoteText = new QTextStream(mNoteData);
+
 }
